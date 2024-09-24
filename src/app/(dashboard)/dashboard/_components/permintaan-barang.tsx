5fc04344da549c1bb7f3dc56f28fe66a6a77d64a
adam <kalmndo@gmail.com>
2024-09-24 19:24:22 +0700

    dashboard

---
+++ src/app/(dashboard)/dashboard/_components/permintaan-barang.tsx
@@ -0,0 +1,116 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function PermintaanBarang({ data }: { data: any }) {
+  return (
+    <>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Persetujuan</CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{data.permintaan}</div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Tersedia</CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{data.perbaikan}</div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Tidak Tersedia
+            </CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <rect width="20" height="14" x="2" y="5" rx="2" />
+              <path d="M2 10h20" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{data.peminjaman}</div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Total Tolak</CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{data.persetujuan}</div>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Aktivitas terkini</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">{/* <Overview /> */}</CardContent>
+        </Card>
+        <Card className="col-span-3">
+          <CardHeader>
+            <CardTitle>Permintaan persetujuan</CardTitle>
+            <CardDescription>You made 265 sales this month.</CardDescription>
+          </CardHeader>
+          <CardContent>{/* <RecentSales /> */}</CardContent>
+        </Card>
+      </div>
+    </>
+  );
+}
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -7,25 +7,65 @@
 // 4. perbaikan
 // 5. peminjaman
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Overview from "./_components/overview";
+import PermintaanBarang from "./_components/permintaan-barang";
+
 // kalau role user tidak ada tabs
 // kalau ada role tabs nya tergantung role
+
+const data = {
+  isUser: false,
+  overview: {
+    permintaan: 2,
+    peminjaman: 3,
+    perbaikan: 5,
+    persetujuan: 3,
+    recent: [{}],
+    persetujuanRecent: [{}],
+  },
+};
 
 export default function Page() {
   return (
     <div>
       <div className="flex justify-between">
         <div className="">
-          <h1 className='text-2xl font-bold tracking-tight'>
-            Welcome, Adam
-          </h1>
-          <p className='text-muted-foreground'>
-            Here is what happened
-          </p>
+          <h1 className="text-2xl font-bold tracking-tight">Welcome, Adam</h1>
+          <p className="text-muted-foreground">Here is what happened</p>
         </div>
       </div>
-      <div className='-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0'>
+      <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
+        {!data.isUser ? (
+          <Tabs defaultValue="overview" className="space-y-4">
+            <TabsList>
+              <TabsTrigger value="overview">Permintaan</TabsTrigger>
+              <TabsTrigger value="permintaan">Permintaan Barang</TabsTrigger>
+              <TabsTrigger value="pengadaan" disabled>
+                Pengadaan
+              </TabsTrigger>
+              <TabsTrigger value="perbaikan" disabled>
+                Perbaikan
+              </TabsTrigger>
+              <TabsTrigger value="peminjaman" disabled>
+                Peminjaman
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="overview" className="space-y-4">
+              <Overview data={data.overview} />
+            </TabsContent>
+            <TabsContent value="permintaan" className="space-y-4">
+              <PermintaanBarang data={data.overview} />
+            </TabsContent>
+          </Tabs>
+        ) : (
+          <div className="space-y-4">
+            <Overview data={data.overview} />
+          </div>
+        )}
         {/* <Table data={data} /> */}
       </div>
     </div>
-  )
+  );
 }
+
