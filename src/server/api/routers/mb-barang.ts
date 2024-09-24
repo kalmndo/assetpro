5536c3399006348c2c45bf34c22456a995d2a276
adam <kalmndo@gmail.com>
2024-09-24 20:19:35 +0700

    user add barang in search

--- src/server/api/routers/mb-barang.ts
+++ src/server/api/routers/mb-barang.ts
@@ -1,52 +1,50 @@
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "@/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { Prisma } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 export const mbBarangRouter = createTRPCRouter({
-  getAll: protectedProcedure
-    .query(async ({ ctx }) => {
-      const result = await ctx.db.masterBarang.findMany({
-        orderBy: {
-          createdAt: "desc"
+  getAll: protectedProcedure.query(async ({ ctx }) => {
+    const result = await ctx.db.masterBarang.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        SubSubKategori: {
+          include: {
+            SubKategori: {
+              include: {
+                Kategori: true,
+              },
+            },
+          },
         },
-        include: {
-          SubSubKategori: {
-            include: {
-              SubKategori: {
-                include: {
-                  Kategori: true
-                }
-              }
-            }
-          }
-        }
-      })
+      },
+    });
 
-      return result.map((v) => ({
-        ...v,
-        kode: `${v.classCode}.${v.code}`,
-        subSubKategori: v.SubSubKategori.name,
-        subKategoriId: v.SubSubKategori.SubKategori.id,
-        kategoriId: v.SubSubKategori.SubKategori.kategoriId,
-        golonganId: v.SubSubKategori.SubKategori.Kategori.golonganId
-      }))
-    }),
+    return result.map((v) => ({
+      ...v,
+      kode: `${v.classCode}.${v.code}`,
+      subSubKategori: v.SubSubKategori.name,
+      subKategoriId: v.SubSubKategori.SubKategori.id,
+      kategoriId: v.SubSubKategori.SubKategori.kategoriId,
+      golonganId: v.SubSubKategori.SubKategori.Kategori.golonganId,
+    }));
+  }),
 
   create: protectedProcedure
-    .input(z.object({
-      name: z.string(),
-      image: z.string().nullable(),
-      code: z.string(),
-      subSubKategoriId: z.string(),
-      classCode: z.string(),
-      uomId: z.string(),
-      deskripsi: z.string(),
-      isUser: z.boolean()
-    }))
+    .input(
+      z.object({
+        name: z.string(),
+        image: z.string().nullable(),
+        code: z.string(),
+        subSubKategoriId: z.string(),
+        classCode: z.string(),
+        uomId: z.string(),
+        deskripsi: z.string(),
+        isUser: z.boolean(),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
       const {
         name,
@@ -56,34 +54,37 @@ export const mbBarangRouter = createTRPCRouter({
         classCode,
         uomId,
         deskripsi,
-        isUser
-      } = input
+        isUser,
+      } = input;
 
+      console.log("code ", code);
+      console.log("isUser ", isUser);
       try {
-
         const transaction = await ctx.db.$transaction(async (tx) => {
-          let theCode = Number(code)
+          let theCode = Number(code);
 
           if (isUser) {
             const masterBarang = await tx.masterBarang.findMany({
               where: {
-                classCode
-              }
-            })
+                classCode,
+              },
+            });
 
-            const codes = masterBarang.map((v) => v.code)
-            // Sort the array to ensure the numbers are in ascending order
-            codes.sort((a, b) => a - b);
+            const codes = masterBarang.map((v) => v.code);
+            if (codes.length > 0) {
+              // Sort the array to ensure the numbers are in ascending order
+              codes.sort((a, b) => a - b);
 
-            // Iterate through the array to find the first missing number
-            for (let i = 0; i < codes.length - 1; i++) {
-              if (codes[i + 1]! - codes[i]! > 1) {
-                theCode = codes[i]! + 1;
+              // Iterate through the array to find the first missing number
+              for (let i = 0; i < codes.length - 1; i++) {
+                if (codes[i + 1]! - codes[i]! > 1) {
+                  theCode = codes[i]! + 1;
+                }
               }
+
+              // If no missing number is found in the loop, return the next number after the maximum
+              theCode = codes[codes.length - 1]! + 1;
             }
-
-            // If no missing number is found in the loop, return the next number after the maximum
-            theCode = codes[codes.length - 1]! + 1;
           }
 
           const result = await tx.masterBarang.create({
@@ -95,52 +96,49 @@ export const mbBarangRouter = createTRPCRouter({
               classCode,
               fullCode: `${classCode}.${theCode}`,
               uomId,
-              deskripsi
+              deskripsi,
             },
             include: {
-              Uom: true
-            }
-          })
+              Uom: true,
+            },
+          });
 
-          if (Number(classCode.split('.')[0]) === 1) {
+          if (Number(classCode.split(".")[0]) === 1) {
             await tx.daftarAsetGroup.create({
               data: {
                 booked: 0,
                 idle: 0,
                 used: 0,
-                id: result.id
-              }
-            })
+                id: result.id,
+              },
+            });
           } else {
             await tx.kartuStok.create({
               data: {
                 qty: 0,
-                id: result.id
-              }
-            })
+                id: result.id,
+              },
+            });
           }
-
-
 
           return {
             ...result,
             kode: result.fullCode,
             kodeAnggaran: [],
-            qty: '1',
+            qty: "1",
             uom: result.Uom.name,
-            golongan: classCode.split('.')[0] === '1' ? 1 : 2
-          }
-
-        })
+            golongan: classCode.split(".")[0] === "1" ? 1 : 2,
+          };
+        });
 
         return {
           ok: true,
-          message: 'Berhasil menambah barang',
-          data: transaction
-        }
+          message: "Berhasil menambah barang",
+          data: transaction,
+        };
       } catch (error) {
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
-          if (error.code === 'P2002') {
+          if (error.code === "P2002") {
             throw new TRPCError({
               code: "INTERNAL_SERVER_ERROR",
               message: "Kode barang ini sudah ada, harap ubah.",
@@ -157,29 +155,23 @@ export const mbBarangRouter = createTRPCRouter({
     }),
 
   update: protectedProcedure
-    .input(z.object({
-      id: z.string(),
-      name: z.string(),
-      image: z.string().nullable(),
-      code: z.string(),
-      subSubKategoriId: z.string(),
-      classCode: z.string()
-    }))
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+        image: z.string().nullable(),
+        code: z.string(),
+        subSubKategoriId: z.string(),
+        classCode: z.string(),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
-      const {
-        id,
-        name,
-        image,
-        code,
-        subSubKategoriId,
-        classCode
-      } = input
+      const { id, name, image, code, subSubKategoriId, classCode } = input;
 
       try {
-
         await ctx.db.masterBarang.update({
           where: {
-            id
+            id,
           },
           data: {
             name,
@@ -187,16 +179,16 @@ export const mbBarangRouter = createTRPCRouter({
             code: Number(code),
             subSubKategoriId,
             classCode,
-            fullCode: `${classCode}.${code}`
+            fullCode: `${classCode}.${code}`,
           },
-        })
+        });
         return {
           ok: true,
-          message: 'Berhasil mengubah barang kategori'
-        }
+          message: "Berhasil mengubah barang kategori",
+        };
       } catch (error) {
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
-          if (error.code === 'P2002') {
+          if (error.code === "P2002") {
             throw new TRPCError({
               code: "INTERNAL_SERVER_ERROR",
               message: "Kode barang ini sudah ada, harap ubah.",
@@ -213,22 +205,22 @@ export const mbBarangRouter = createTRPCRouter({
     }),
 
   remove: protectedProcedure
-    .input(z.object({
-      id: z.string(),
-    }))
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
     .mutation(async ({ ctx, input }) => {
-      const {
-        id,
-      } = input
+      const { id } = input;
 
       try {
         await ctx.db.masterBarang.delete({
           where: { id },
-        })
+        });
         return {
           ok: true,
-          message: 'Berhasil menghapus barang'
-        }
+          message: "Berhasil menghapus barang",
+        };
       } catch (error) {
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
@@ -237,5 +229,4 @@ export const mbBarangRouter = createTRPCRouter({
         });
       }
     }),
-
 });
