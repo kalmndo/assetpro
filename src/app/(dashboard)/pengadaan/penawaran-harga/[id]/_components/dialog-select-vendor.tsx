fd039796d17036e323e81300d14d7797ca4d23ab
adam <kalmndo@gmail.com>
2024-09-26 11:15:11 +0700

    bisa input penawaran harga catatan, delivery, termin, garansi

--- src/app/(dashboard)/pengadaan/penawaran-harga/[id]/_components/dialog-select-vendor.tsx
+++ src/app/(dashboard)/pengadaan/penawaran-harga/[id]/_components/dialog-select-vendor.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import {
   Dialog,
   DialogContent,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { type Dispatch, type SetStateAction, useState, useEffect } from "react";
 import { DataTable } from "@/components/data-table";
@@ -15,100 +15,126 @@ import { CurrencyInput } from "@/components/currency-input";
 import { type Row } from "@tanstack/react-table";
 import { type RouterOutputs } from "@/trpc/react";
 import { STATUS } from "@/lib/status";
+import { Input } from "@/components/ui/input";
 
-
-const renderSubComponent = ({ row }: { row: Row<RouterOutputs['penawaranHarga']['get']['barang'][0]['vendor'][0]> }) => {
+const renderSubComponent = ({
+  row,
+}: {
+  row: Row<RouterOutputs["penawaranHarga"]["get"]["barang"][0]["vendor"][0]>;
+}) => {
   return (
-    <div className="flex flex-col space-y-2">
-      <div >
-        <p className="font-semibold">Catatan</p>
-        <p className="text-sm">{row.original.catatan}</p>
+    <div className="grid grid-cols-12 gap-4">
+      <div className="col-span-4 space-y-4">
+        <div>
+          <p className="font-semibold">Termin pembayaran </p>
+          <p className="text-sm">{row.original.termin}</p>
+        </div>
+        <div>
+          <p className="font-semibold">Waktu pengiriman</p>
+          <p className="text-sm">{row.original.delivery}</p>
+        </div>
       </div>
-      <div >
-        <p className="font-semibold">Termin pembayaran & waktu pengiriman</p>
-        <p className="text-sm">{row.original.termin}</p>
-      </div>
-      <div >
-        <p className="font-semibold">Garansi</p>
-        <p className="text-sm">{row.original.garansi}</p>
+      <div className="col-span-4 space-y-4">
+        <div>
+          <p className="font-semibold">Garansi</p>
+          <p className="text-sm">{row.original.garansi}</p>
+        </div>
+        <div>
+          <p className="font-semibold">Catatan</p>
+          <p className="text-sm">{row.original.catatan}</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default function DialogSelectVendor({
   data,
   open,
+  // eslint-disable-next-line
   onOpenChange,
-  // @ts-ignore
+  // @ts-ignore eslint-disable-next-line
   setBarang,
   status,
-  canSend
+  canSend,
 }: {
-  data: any,
-  status: string
-  canSend: boolean
+  data: any;
+  status: string;
+  canSend: boolean;
   // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
-  open: boolean,
-  onOpenChange(): void,
-  setBarang: Dispatch<SetStateAction<{
-    id: string;
-    name: string;
-    kode: string;
-    image: string;
-    uom: string;
-    qty: number;
-    hargaNego: number;
-    jumlahVendor: number;
-    vendor: {
-      name: string;
-      harga: number | null;
-      total: number | null;
-      catatan: string | null,
-      garansi: string | null,
-      termin: string | null
-    }[];
-  }[]>>
+  open: boolean;
+  onOpenChange(): void;
+  setBarang: Dispatch<
+    SetStateAction<
+      {
+        id: string;
+        name: string;
+        kode: string;
+        image: string;
+        uom: string;
+        qty: number;
+        hargaNego: number;
+        jumlahVendor: number;
+        vendor: {
+          name: string;
+          harga: number | null;
+          total: number | null;
+          catatan: string | null;
+          garansi: string | null;
+          termin: string | null;
+          delivery: string | null;
+        }[];
+      }[]
+    >
+  >;
 }) {
-  const [harga, setHarga] = useState(data.hargaNego)
+  const [harga, setHarga] = useState(data.hargaNego);
+  const [termin, setTermin] = useState(data.hargaNego);
+  const [delivery, setDelivery] = useState(data.hargaNego);
+  const [garansi, setGaransi] = useState(data.hargaNego);
+  const [catatan, setCatatan] = useState(data.hargaNego);
 
   const onChange = (e: any) => {
-    setHarga(e)
-  }
+    setHarga(e);
+  };
 
   const onSubmit = () => {
-
     setBarang((prev) => {
       return prev.map((v) => {
         if (v.id === data.id) {
           return {
             ...v,
-            hargaNego: Number(harga)
-          }
-
+            hargaNego: Number(harga),
+            termin,
+            delivery,
+            garansi,
+            catatan,
+          };
         }
-        return v
-      })
-    })
-    onOpenChange()
-  }
+        return v;
+      });
+    });
+    onOpenChange();
+  };
 
   useEffect(() => {
     if (open) {
-      setHarga(data.hargaNego)
+      setHarga(data.hargaNego);
     } else {
-      setHarga(0)
+      setHarga(0);
     }
-
-  }, [open, setHarga, data])
+  }, [open, setHarga, data]);
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-6xl">
+      <DialogContent className="max-h-screen overflow-y-scroll sm:max-w-6xl">
         <DialogHeader>
           <DialogTitle>Penawaran Harga</DialogTitle>
         </DialogHeader>
-        <p>Silahkan input harga negosiasi untuk barang <span className="font-semibold">{data.name}</span></p>
+        <p>
+          Silahkan input harga negosiasi untuk barang{" "}
+          <span className="font-semibold">{data.name}</span>
+        </p>
         <div className="flex justify-between">
           <div className="space-y-1">
             <Label className="font-semibold">Harga penawaran</Label>
@@ -119,9 +145,55 @@ export default function DialogSelectVendor({
               disabled={!canSend || status === STATUS.SELESAI.id}
             />
           </div>
+          <div className="space-y-1">
+            <Label className="font-semibold">Termin pembayaran</Label>
+            <Input
+              value={termin}
+              placeholder="Termin"
+              onChange={(e) => {
+                setTermin(e.target.value);
+              }}
+              disabled={!canSend || status === STATUS.SELESAI.id}
+            />
+          </div>
+          <div className="space-y-1">
+            <Label className="font-semibold">Waktu pengiriman</Label>
+            <Input
+              value={delivery}
+              placeholder="Waktu pengiriman"
+              onChange={(e) => {
+                setDelivery(e.target.value);
+              }}
+              disabled={!canSend || status === STATUS.SELESAI.id}
+            />
+          </div>
+          <div className="space-y-1">
+            <Label className="font-semibold">Garansi</Label>
+            <Input
+              value={garansi}
+              placeholder="Garansi"
+              onChange={(e) => {
+                setGaransi(e.target.value);
+              }}
+              disabled={!canSend || status === STATUS.SELESAI.id}
+            />
+          </div>
           <div className="space-y-1 text-right">
             <Label className="font-semibold">Harga penawaran total</Label>
             <p>Rp {(data.qty * harga).toLocaleString("id-ID")}</p>
+          </div>
+        </div>
+        <div className="w-[400px]">
+          <div className="space-y-1">
+            <Label className="font-semibold">Catatan</Label>
+            <Input
+              value={catatan}
+              placeholder="Catatan"
+              onChange={(e) => {
+                setCatatan(e.target.value);
+              }}
+              disabled={!canSend || status === STATUS.SELESAI.id}
+            />
           </div>
         </div>
         <DataTable
@@ -136,8 +208,8 @@ export default function DialogSelectVendor({
         />
 
         <DialogFooter>
-          {
-            status !== STATUS.SELESAI.id && <Button
+          {status !== STATUS.SELESAI.id && (
+            <Button
               type="submit"
               size="lg"
               onClick={onSubmit}
@@ -145,9 +217,9 @@ export default function DialogSelectVendor({
             >
               Simpan
             </Button>
-          }
+          )}
         </DialogFooter>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
--- src/server/api/routers/penawaran-harga.ts
+++ src/server/api/routers/penawaran-harga.ts
@@ -3,54 +3,55 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { z } from "zod";
 import { STATUS } from "@/lib/status";
 import sendWhatsAppMessage from "@/lib/send-whatsapp";
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 import formatPhoneNumber from "@/lib/formatPhoneNumber";
 import isTodayOrAfter from "@/lib/isTodayOrAfter";
 // import isTodayOrAfter from "@/lib/isTodayOrAfter";
 
 export const penawaranHargaRouter = createTRPCRouter({
-  getAll: protectedProcedure
-    .query(async ({ ctx }) => {
-      const result = await ctx.db.penawaranHarga.findMany({
-        orderBy: {
-          createdAt: "desc"
+  getAll: protectedProcedure.query(async ({ ctx }) => {
+    const result = await ctx.db.penawaranHarga.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        PermintaanPenawaran: {
+          include: {
+            Pembelian: {
+              include: {
+                PermintaanPembelianBarang: true,
+              },
+            },
+          },
         },
-        include: {
-          PermintaanPenawaran: {
-            include: {
-              Pembelian: {
-                include: {
-                  PermintaanPembelianBarang: true
-                }
-              }
-            }
-          }
-        }
-      })
+      },
+    });
 
-      if (!result) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "Tidak ada form ini",
-        });
-      }
+    if (!result) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Tidak ada form ini",
+      });
+    }
 
-      return result?.map((v) => ({
-        ...v,
-        jumlah: v.PermintaanPenawaran.Pembelian.PermintaanPembelianBarang.length,
-        tanggal: v.createdAt.toLocaleDateString()
-      }))
-    }),
+    return result?.map((v) => ({
+      ...v,
+      jumlah: v.PermintaanPenawaran.Pembelian.PermintaanPembelianBarang.length,
+      tanggal: v.createdAt.toLocaleDateString(),
+    }));
+  }),
   get: protectedProcedure
-    .input(z.object({
-      id: z.string()
-    }))
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
     .query(async ({ ctx, input }) => {
-      const { id } = input
+      const { id } = input;
 
       const result = await ctx.db.penawaranHarga.findFirst({
         where: {
-          id
+          id,
         },
         include: {
           PermintaanPenawaran: {
@@ -65,24 +66,24 @@ export const penawaranHargaRouter = createTRPCRouter({
                         include: {
                           Vendor: {
                             include: {
-                              Vendor: true
-                            }
-                          }
-                        }
+                              Vendor: true,
+                            },
+                          },
+                        },
                       },
                       MasterBarang: {
                         include: {
-                          Uom: true
-                        }
+                          Uom: true,
+                        },
                       },
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      })
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
 
       if (!result) {
         throw new TRPCError({
@@ -91,85 +92,93 @@ export const penawaranHargaRouter = createTRPCRouter({
         });
       }
 
-      let getVendors
+      let getVendors;
 
       if (result.status === STATUS.MENUNGGU.id) {
-        getVendors = await ctx.db.vendor.findMany()
+        getVendors = await ctx.db.vendor.findMany();
       }
 
-      const barang = result.PermintaanPenawaran.Pembelian.PermintaanPembelianBarang.map((v) => ({
-        id: v.id,
-        name: v.MasterBarang.name,
-        kode: v.MasterBarang.fullCode,
-        image: v.MasterBarang.image ?? '',
-        uom: v.MasterBarang.Uom.name,
-        qty: v.qty,
-        hargaNego: v.PenawaranHargaBarangNego?.hargaNego ?? 0,
-        jumlahVendor: v.PermintaanPenawaranBarangVendor.length,
-        vendor: v.PermintaanPenawaranBarangVendor.map((a) => ({
-          name: a.Vendor.Vendor.name,
-          harga: a.harga,
-          total: a.totalHarga,
-          catatan: a.catatan,
-          garansi: a.garansi,
-          termin: a.termin
-        })),
-      }))
+      const barang =
+        result.PermintaanPenawaran.Pembelian.PermintaanPembelianBarang.map(
+          (v) => ({
+            id: v.id,
+            name: v.MasterBarang.name,
+            kode: v.MasterBarang.fullCode,
+            image: v.MasterBarang.image ?? "",
+            uom: v.MasterBarang.Uom.name,
+            qty: v.qty,
+            hargaNego: v.PenawaranHargaBarangNego?.hargaNego ?? 0,
+            jumlahVendor: v.PermintaanPenawaranBarangVendor.length,
+            vendor: v.PermintaanPenawaranBarangVendor.map((a) => ({
+              name: a.Vendor.Vendor.name,
+              harga: a.harga,
+              total: a.totalHarga,
+              catatan: a.catatan,
+              garansi: a.garansi,
+              termin: a.termin,
+              delivery: a.delivery,
+            })),
+          }),
+        );
 
       return {
         id: result.id,
         no: result.no,
         permintaanPenawaran: {
           id: result.PermintaanPenawaran.id,
-          no: result.PermintaanPenawaran.no
+          no: result.PermintaanPenawaran.no,
         },
         barang,
         status: result.status,
         tanggal: result.createdAt.toLocaleDateString(),
         getVendors: getVendors ?? [],
-        canSend: isTodayOrAfter(result.PermintaanPenawaran.deadline) && result.status === STATUS.MENUNGGU.id,
-        penawaranDeadline: result.PermintaanPenawaran.deadline?.toLocaleDateString(),
-        deadline: result.deadline?.toLocaleDateString()
-      }
+        canSend:
+          isTodayOrAfter(result.PermintaanPenawaran.deadline) &&
+          result.status === STATUS.MENUNGGU.id,
+        penawaranDeadline:
+          result.PermintaanPenawaran.deadline?.toLocaleDateString(),
+        deadline: result.deadline?.toLocaleDateString(),
+      };
     }),
   send: protectedProcedure
     .input(
       z.object({
         id: z.string(),
         deadline: z.date(),
-        barang: z.array(z.object({
-          id: z.string({ description: "Permintaan pembelian barang id" }),
-          hargaNego: z.number()
-        }))
-      })
+        barang: z.array(
+          z.object({
+            id: z.string({ description: "Permintaan pembelian barang id" }),
+            hargaNego: z.number(),
+            catatan: z.string().optional(),
+            termin: z.string().optional(),
+            delivery: z.string().optional(),
+            garansi: z.string().optional(),
+          }),
+        ),
+      }),
     )
     .mutation(async ({ ctx, input }) => {
-      const {
-        id,
-        deadline,
-        barang
-      } = input
+      const { id, deadline, barang } = input;
 
       try {
-
         await ctx.db.$transaction(async (tx) => {
           const penawaranResult = await tx.penawaranHarga.update({
             where: {
-              id
+              id,
             },
             data: {
               deadline,
-              status: STATUS.SELESAI.id
-            }
-          })
+              status: STATUS.SELESAI.id,
+            },
+          });
 
-          const permintaanPembelianBarangIds = barang.map((v) => v.id)
+          const permintaanPembelianBarangIds = barang.map((v) => v.id);
 
           const pBSPBB = await tx.pBSPBB.findMany({
             where: {
-              pembelianBarangId: { in: permintaanPembelianBarangIds }
-            }
-          })
+              pembelianBarangId: { in: permintaanPembelianBarangIds },
+            },
+          });
 
           for (const { barangSplitId } of pBSPBB) {
             await tx.permintaanBarangBarangSplitHistory.create({
@@ -177,34 +186,45 @@ export const penawaranHargaRouter = createTRPCRouter({
                 formType: "penawaran-harga",
                 barangSplitId,
                 formNo: penawaranResult.no,
-                desc: "Permintaan penawaran harga ke vendor"
-              }
-            })
+                desc: "Permintaan penawaran harga ke vendor",
+              },
+            });
           }
 
-          for (const { id, hargaNego } of barang) {
+          for (const {
+            id,
+            hargaNego,
+            termin,
+            catatan,
+            garansi,
+            delivery,
+          } of barang) {
             await tx.penawaranHargaBarangNego.create({
               data: {
                 pembelianBarangId: id,
-                hargaNego
-              }
-            })
+                hargaNego,
+                termin,
+                catatan,
+                garansi,
+                delivery,
+              },
+            });
           }
 
           const asdf = await tx.permintaanPenawaranBarangVendor.findMany({
             where: {
               pembelianBarangId: {
-                in: barang.map((v) => v.id)
+                in: barang.map((v) => v.id),
               },
             },
             include: {
               Vendor: {
                 include: {
-                  Vendor: true
-                }
-              }
-            }
-          })
+                  Vendor: true,
+                },
+              },
+            },
+          });
 
           const groupByPembelianBarangId = () => {
             return asdf.reduce((acc, item) => {
@@ -232,12 +252,15 @@ export const penawaranHargaRouter = createTRPCRouter({
                 // Sort items by harga in ascending order, ignoring null values
                 const sortedItems = items
                   .filter((item: any) => item.harga !== null)
-                  .sort((a: any, b: any) => (a.harga as number) - (b.harga as number));
+                  .sort(
+                    (a: any, b: any) =>
+                      (a.harga as number) - (b.harga as number),
+                  );
 
                 // Get the three lowest prices
                 result[key] = sortedItems.slice(0, 3).map((v: any) => ({
                   ...v,
-                  vendor: v.Vendor.Vendor
+                  vendor: v.Vendor.Vendor,
                 }));
               }
             }
@@ -252,15 +275,17 @@ export const penawaranHargaRouter = createTRPCRouter({
 
             for (const key in lowestPrices) {
               // @ts-ignore
-              lowestPrices[key].forEach(item => {
+              lowestPrices[key].forEach((item) => {
                 const vendor = item.vendor;
                 if (!vendorsMap.has(vendor.id)) {
                   vendorsMap.set(vendor.id, {
                     ...vendor,
-                    pembelianBarangId: []
+                    pembelianBarangId: [],
                   });
                 }
-                vendorsMap.get(vendor.id).pembelianBarangId.push(item.pembelianBarangId);
+                vendorsMap
+                  .get(vendor.id)
+                  .pembelianBarangId.push(item.pembelianBarangId);
               });
             }
 
@@ -268,7 +293,7 @@ export const penawaranHargaRouter = createTRPCRouter({
           }
 
           for (const { id, pembelianBarangId } of getUniqueVendors()) {
-            const url = uuidv4()
+            const url = uuidv4();
 
             const result = await tx.penawaranHargaVendor.create({
               data: {
@@ -279,35 +304,40 @@ export const penawaranHargaRouter = createTRPCRouter({
                   createMany: {
                     data: pembelianBarangId.map((v: string) => ({
                       pembelianBarangId: v,
-                    }))
-                  }
-                }
+                    })),
+                  },
+                },
               },
               include: {
                 PenawaranHargaBarangVendor: {
                   include: {
                     PembelianBarang: {
                       include: {
-                        MasterBarang: true
-                      }
-                    }
-                  }
+                        MasterBarang: true,
+                      },
+                    },
+                  },
                 },
-                Vendor: true
-              }
-            })
-            const barang = result.PenawaranHargaBarangVendor.map((v) => v.PembelianBarang.MasterBarang.name)
+                Vendor: true,
+              },
+            });
+            const barang = result.PenawaranHargaBarangVendor.map(
+              (v) => v.PembelianBarang.MasterBarang.name,
+            );
 
             const message = `
           *ASSETPRO - YAYASAN ALFIAN HUSIN*
 
 Melakukan penawaran harga pada barang.
-${barang.map((v, i) => `${i + 1}. ${v}`).join('\n')}
+${barang.map((v, i) => `${i + 1}. ${v}`).join("\n")}
 
 Silahkan klik link berikut untuk mengirim penawaran harga.
-https://assetpro.site/vendor/ph/${result.id}`
+https://assetpro.site/vendor/ph/${result.id}`;
 
-            sendWhatsAppMessage(formatPhoneNumber(result.Vendor.whatsapp), message)
+            sendWhatsAppMessage(
+              formatPhoneNumber(result.Vendor.whatsapp),
+              message,
+            );
           }
           // EVALUASI CREATE
           const evaluasi = await tx.evaluasi.create({
@@ -316,28 +346,26 @@ https://assetpro.site/vendor/ph/${result.id}`
               status: STATUS.PENGAJUAN.id,
               penawaranHargaId: penawaranResult.id,
             },
-          })
+          });
 
           await tx.evaluasiBarang.createMany({
             data: barang.map((v) => ({
               evaluasiId: evaluasi.id,
-              pembelianBarangId: v.id
-            }))
-          })
-        })
+              pembelianBarangId: v.id,
+            })),
+          });
+        });
 
         return {
           ok: true,
-          message: 'Berhasil mengirim harga penawaran'
-        }
-
+          message: "Berhasil mengirim harga penawaran",
+        };
       } catch (error) {
         throw new TRPCError({
           code: "NOT_FOUND",
           message: "Tidak ada form ini",
-          cause: error
+          cause: error,
         });
       }
-    })
-})
-
+    }),
+});
